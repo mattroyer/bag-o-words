a1fa9bdeb2f94629d1435adb4231ec4550d131e5
Matthew Royer <matthew.royer@gmail.com>
2015-03-18 09:54:42 -0600

    Made some needed refactoring changes, that's to @ryancastro

--- bag_of_words.rb
+++ bag_of_words.rb
@@ -1,9 +1,24 @@
 class BagOfWords
+  attr_reader :bag
   def initialize(docs)
     @docs = docs
     @bag = []
 
     magic
+  end
+
+  def dictionary
+    Hash[unique_items.zip (1..unique_items.size)]
+  end
+
+  private
+
+  def magic
+    @docs.map! { |doc| doc.gsub(/[^\w| ]/, '').split }
+
+    @docs.each do |doc|
+      @bag << dictionary.map { |key, value| doc.select { |item| item == key }.count }
+    end
   end
 
   def words_only
@@ -14,19 +29,4 @@ class BagOfWords
     words_only.uniq
   end
 
-  def dictionary
-    Hash[unique_items.zip (1..unique_items.size)]
-  end
-
-  def magic
-    @docs.map! { |doc| doc.gsub(/[^\w| ]/, '').split }
-
-    @docs.each do |doc|
-      @bag << dictionary.map { |key, value| doc.select { |item| item == key }.count }
-    end
-  end
-
-  def bag
-    @bag
-  end
 end
